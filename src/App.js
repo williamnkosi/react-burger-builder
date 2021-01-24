f653bfb1c336ef0081030f5d55f2cc7b80f34906
williamnkosi <william.acc.dev@gmail.com>
2021-01-24 12:14:25 -0600

    setup the project folders and files structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 
+import Layout from './componenets/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+
 
 class App extends Component {
   render() {
     return (
       <div >
-
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
     );
   }
