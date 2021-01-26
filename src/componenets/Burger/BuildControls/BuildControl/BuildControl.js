288bc84ee7f75fe59e3a987c4364165bedec82aa
williamnkosi <william.acc.dev@gmail.com>
2021-01-25 22:09:33 -0600

    finished the burger builder controls

--- src/componenets/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/componenets/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Classes from './BuildControl.css';
 
-const BuildControl = () => {
+const BuildControl = (props) => {
     return (
         <div className={Classes.BuildControl}>
-            <div className={Classes.Label}>{props.lable}</div>
-            <button className={Classes.Less}>Less</button>
-            <button className={Classes.More}>More</button>
+            <div className={Classes.Label}>{props.label}</div>
+            <button className={Classes.Less}
+                onClick={props.removed}
+                disabled={props.disabled}>Less</button>
+            <button className={Classes.More}
+                onClick={props.added}
+            >More</button>
         </div>
     )
 }
--- src/componenets/Burger/BuildControls/BuildControls.js
+++ src/componenets/Burger/BuildControls/BuildControls.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import Classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl'
 
-const BuildControls = () => (
-    <div>
+const controls = [
+    { label: 'salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' },
+]
 
+const BuildControls = (props) => (
+    <div className={Classes.BuildControls}>
+        <p>Current Price: {props.price.toFixed(2)}</p>
+        {controls.map(ctrl => (
+            <BuildControl
+                key={ctrl.label}
+                label={ctrl.label}
+                added={() => props.ingredientAdded(ctrl.type)}
+                removed={() => props.ingredientRemoved(ctrl.type)}
+                disabled={props.disabled[ctrl.type]}></BuildControl>
+        ))}
     </div>
 )
 
